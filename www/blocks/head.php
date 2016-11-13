44c858204bdcc71d040892ece499850b5f84b2ad
Bismila <bismiladance@gmail.com>
2016-11-14 00:51:38 +0200

    подключение необходимых файлов и библиотек
    добавлены jquery файл для dropmenu статей и соответствующая библиотека

---
+++ www/blocks/head.php
@@ -0,0 +1,9 @@
+
+<meta charset="utf-8" />
+<title><?=$title ?></title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript" src="js/dropmenu.js"></script>
+<?php
+require_once "functions/connectDB.php";
+?>
