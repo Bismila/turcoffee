3e2d84c50e3892cb2c059179d54cc92a2ce9cb5f
Bismila <bismiladance@gmail.com>
2016-11-14 00:52:16 +0200

    Стили
    Подправлены стили под резину

---
+++ www/blocks/header.php
@@ -0,0 +1,20 @@
+<header>
+    <div id="logo">Кофе по-турецки</div>
+    <div>
+        <a href="contacts.php">
+            <div id="menuHead">Контакты</div>
+        </a>
+        <a href="news.php">
+            <div id="menuHead">Новости</div>
+        </a>
+        <a href="gallery.php">
+            <div id="menuHead">Галерея</div>
+        </a>
+        <a href="aboutUs.php">
+            <div id="menuHead">О нас</div>
+        </a>
+        <a href="index.php">
+            <div id="menuHead">Главная</div>
+        </a>
+    </div>
+</header>
