6530e3304104a20b4a83c0635f9d416d34e0aecd
Bismila <bismiladance@gmail.com>
2016-11-14 00:51:54 +0200

    Стили
    подправлены стили

---
+++ www/blocks/footer.php
@@ -0,0 +1,34 @@
+
+<footer>
+	<div id="">
+		<a href="contacts.php">
+			<div id="menuFooter">Контакты</div>
+		</a>
+		<a href="news.php">
+			<div id="menuFooter">Новости</div>
+		</a>
+		<a href="gallery.php">
+			<div id="menuFooter">Галерея</div>
+		</a>
+		<a href="aboutUs.php">
+			<div id="menuFooter">О нас</div>
+		</a>
+		<a href="index.php">
+			<div id="menuFooter">Главная</div>
+		</a>
+	</div>
+	<div id="social">
+		<a href="http://vk.com" title="Группа vk" target="_blank">
+			<img src="img/vk.png" alt="vk" title="vk" />
+		</a>
+		<a href="http://facebook.com" title="Группа facebook" target="_blank">
+			<img src="img/facebook.png" alt="facebook" title="facebook" />
+		</a>
+		<a href="http://twitter.com" title="Группа twitter" target="_blank">
+			<img src="img/twitter.png" alt="twitter" title="twitter" />
+		</a>
+	</div>
+	<div id="rights">
+		Все права защищены &copy; <?php echo date('Y') ?>
+	</div>
+</footer>
